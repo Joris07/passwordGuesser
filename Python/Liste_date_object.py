a19ffa94edfcf38da42377d41074ef5d03f17389
Joris07 <joris.gourdon07@gmail.com>
2023-04-12 12:44:29 +0200

    Changement sur les dates

--- Python/Liste_date_object.py
+++ Python/Liste_date_object.py
@@ -1,4 +1,6 @@
 from Config_object import Config
+from datetime import datetime
+from babel.dates import format_date, format_datetime
 
 class Liste_date:
 	def __init__(self, liste_initiale):
@@ -6,27 +8,32 @@ class Liste_date:
 		self.liste_finale = liste_initiale[:]
 		self.config = Config()
 
-	def getListeInitiale(self):
+	def fct_getListeInitiale(self):
 		return self.liste_initiale
 	
-	def getListeFinale(self):
+	def fct_getListeFinale(self):
 		return self.liste_finale
 	
-	def extractDaysOrMonthsOrYears(self, index): # 0 -> Année | 1 -> Mois | 2 -> Jour
+	def fct_extractDaysOrMonthsOrYears(self, index): # 0 -> Année | 1 -> Mois | 2 -> Jour
 		for date_1 in self.liste_initiale:
 			self.liste_finale.append(date_1.split("-")[index])
 
-	def extractYearIn2Digits(self):
+	def fct_extractYearIn2Digits(self):
 		for date in self.liste_initiale:
 			self.liste_finale.append(date.split("-")[0][-2]+date.split("-")[0][-1])
 	
-	def separateDateNumbers(self):
+	def fct_separateDateNumbers(self):
 		for date in self.liste_initiale:
 			self.liste_finale += date.split("-")
 
-	def returnMonthInLetters(self):
+	def fct_monthInLetters(self, lang):
 		for date in self.liste_initiale:
-			monthNumber = date.split("-")[1]
-			if monthNumber in self.config.months_map:
-				self.liste_finale.append(self.config.months_map[monthNumber])
+			date_obj = datetime.strptime(date, '%Y-%m-%d')
+			month_formatted = format_date(date_obj, format='MMMM', locale=lang)
+			self.liste_finale.append(month_formatted)
     
+	def fct_daysInLetters(self, lang):
+		for date in self.liste_initiale:
+			date_obj = datetime.strptime(date, '%Y-%m-%d')
+			weekday_formatted = format_datetime(date_obj, format='EEEE', locale=lang)
+			self.liste_finale.append(weekday_formatted)
